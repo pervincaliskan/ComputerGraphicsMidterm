651ed75d06aae305f569c87d062cb18396848d66
pervincaliskan <pervincaliskan01@hotmail.com>
2022-01-06 23:24:10 +0300

    Ödev dosyaları eklendi

---
+++ PervinCaliskan/PervinCaliskan/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+namespace PervinCaliskan
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.FlowLayoutPanel();
+            this.pbInput = new System.Windows.Forms.PictureBox();
+            this.pbOutput = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.buton1 = new System.Windows.Forms.Button();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOutput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.AutoScroll = true;
+            this.panel.AutoSize = true;
+            this.panel.Controls.Add(this.pbInput);
+            this.panel.Controls.Add(this.pbOutput);
+            this.panel.Controls.Add(this.pictureBox1);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Margin = new System.Windows.Forms.Padding(4);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1281, 308);
+            this.panel.TabIndex = 3;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // pbInput
+            // 
+            this.pbInput.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pbInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbInput.Location = new System.Drawing.Point(4, 4);
+            this.pbInput.Margin = new System.Windows.Forms.Padding(4);
+            this.pbInput.MinimumSize = new System.Drawing.Size(100, 100);
+            this.pbInput.Name = "pbInput";
+            this.pbInput.Size = new System.Drawing.Size(401, 300);
+            this.pbInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbInput.TabIndex = 3;
+            this.pbInput.TabStop = false;
+            this.pbInput.Click += new System.EventHandler(this.pbInput_Click);
+            // 
+            // pbOutput
+            // 
+            this.pbOutput.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pbOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbOutput.Location = new System.Drawing.Point(413, 4);
+            this.pbOutput.Margin = new System.Windows.Forms.Padding(4);
+            this.pbOutput.MinimumSize = new System.Drawing.Size(100, 100);
+            this.pbOutput.Name = "pbOutput";
+            this.pbOutput.Size = new System.Drawing.Size(422, 300);
+            this.pbOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbOutput.TabIndex = 4;
+            this.pbOutput.TabStop = false;
+            this.pbOutput.Click += new System.EventHandler(this.pbOutput_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(843, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(422, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.progressBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.progressBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.progressBar.Location = new System.Drawing.Point(0, 366);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(1261, 31);
+            this.progressBar.TabIndex = 5;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnOpen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOpen.ForeColor = System.Drawing.Color.White;
+            this.btnOpen.Location = new System.Drawing.Point(106, 321);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(90, 37);
+            this.btnOpen.TabIndex = 6;
+            this.btnOpen.Text = "Şifrele";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // buton1
+            // 
+            this.buton1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buton1.ForeColor = System.Drawing.Color.White;
+            this.buton1.Location = new System.Drawing.Point(525, 321);
+            this.buton1.Margin = new System.Windows.Forms.Padding(4);
+            this.buton1.Name = "buton1";
+            this.buton1.Size = new System.Drawing.Size(83, 37);
+            this.buton1.TabIndex = 7;
+            this.buton1.Text = "Ayikla";
+            this.buton1.UseVisualStyleBackColor = false;
+            this.buton1.Click += new System.EventHandler(this.buton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1281, 406);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.buton1);
+            this.Controls.Add(this.btnOpen);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Pervin Çalışkan";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOutput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel panel;
+        private System.Windows.Forms.PictureBox pbOutput;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.PictureBox pbInput;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button buton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ PervinCaliskan/PervinCaliskan/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PervinCaliskan
+{
+    public partial class Form1 : Form
+    {
+
+        private string fileo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public struct Coord
+        {
+            public int x, y;
+        }
+
+        private void Work(Bitmap srcb, Bitmap outb, PictureBox yeni)
+        {
+            int w = srcb.Width, h = srcb.Height;
+            Coord[] coord = new Coord[w * h];
+
+            FastBitmap fsb = new FastBitmap(srcb);
+            FastBitmap fob = new FastBitmap(outb);
+            fsb.LockImage();
+            fob.LockImage();
+            ulong seed = 0;
+            int numpix = 0;
+            for (int y = 0; y < h; y++)
+                for (int x = 0; x < w; numpix++, x++)
+                {
+                    coord[numpix].x = x;
+                    coord[numpix].y = y;
+                    uint color = fsb.GetPixel(x, y);
+                    seed += color;
+                    fob.SetPixel(x, y, color);
+                }
+            fsb.UnlockImage();
+            fob.UnlockImage();
+
+            Application.DoEvents();
+
+            int half = numpix / 2;
+            int limit = half;
+            XorShift rng = new XorShift(seed);
+            progressBar.Visible = true;
+            progressBar.Maximum = limit;
+
+            fob.LockImage();
+            while (limit > 0)
+            {
+                int p = (int)(rng.next() % (uint)limit);
+                int q = (int)(rng.next() % (uint)limit);
+                uint color = fob.GetPixel(coord[p].x, coord[p].y);
+                fob.SetPixel(coord[p].x, coord[p].y, fob.GetPixel(coord[half + q].x, coord[half + q].y));
+                fob.SetPixel(coord[half + q].x, coord[half + q].y, color);
+                limit--;
+                if (p < limit)
+                {
+                    coord[p] = coord[limit];
+                }
+                if (q < limit)
+                {
+                    coord[half + q] = coord[half + limit];
+                }
+                if ((limit & 0xfff) == 0)
+                {
+                    progressBar.Value = limit;
+                    fob.UnlockImage();
+                    yeni.Refresh();
+                    fob.LockImage();
+                }
+            }
+            fob.UnlockImage();
+            yeni.Refresh();
+            progressBar.Visible = false;
+        }
+
+        void DupImage(PictureBox s, PictureBox d)
+        {
+            if (d.Image != null)
+                d.Image.Dispose();
+            d.Image = new Bitmap(s.Image.Width, s.Image.Height);
+        }
+
+        void GetImagePB(PictureBox pb, string file)
+        {
+            Bitmap bms = new Bitmap(file, false);
+            Bitmap bmp = bms.Clone(new Rectangle(0, 0, bms.Width, bms.Height), PixelFormat.Format32bppArgb);
+            bms.Dispose();
+            if (pb.Image != null)
+                pb.Image.Dispose();
+            pb.Image = bmp;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\temp\\";
+            openFileDialog.Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string file = openFileDialog.FileName;
+
+                    GetImagePB(pbInput, file);
+                    pbInput.Tag = file;
+                    DupImage(pbInput, pbOutput);
+                    Work(pbInput.Image as Bitmap, pbOutput.Image as Bitmap, pbOutput);
+                    file = Path.GetDirectoryName(file) + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(file) + ".scr.png";
+                    pbOutput.Image.Save(file);
+                    pbOutput.Refresh();
+                    fileo = file;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+
+            }
+        }
+
+        private void pbInput_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buton1_Click(object sender, EventArgs e)
+        {
+
+            if (fileo != null)
+            {
+                try
+                {
+                    string file = fileo;
+                    GetImagePB(pbOutput, file);
+                    pictureBox1.Tag = file;
+                    DupImage(pbOutput, pictureBox1);
+                    Work(pbOutput.Image as Bitmap, pictureBox1.Image as Bitmap, pictureBox1);
+                    file = Path.GetDirectoryName(file) + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(file) + ".scr.png";
+                    pictureBox1.Image.Save(file);
+                    pictureBox1.Refresh();
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+
+            }
+        }
+
+
+        private void pbOutput_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\temp\\";
+            openFileDialog.Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string file = openFileDialog.FileName;
+                    GetImagePB(pbOutput, file);
+                    pbInput.Tag = file;
+                    DupImage(pbOutput, pictureBox1);
+                    Work(pbOutput.Image as Bitmap, pictureBox1.Image as Bitmap, pbOutput);
+                    file = Path.GetDirectoryName(file) + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(file) + ".scr.png";
+                    pictureBox1.Image.Save(file);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+
+            }
+        }
+
+    }
+
+
+
+    unsafe public class FastBitmap
+    {
+        private Bitmap workingBitmap = null;
+        private int width = 0;
+        private BitmapData bitmapData = null;
+        private Byte* pBase = null;
+
+        public FastBitmap(Bitmap inputBitmap)
+        {
+            workingBitmap = inputBitmap;
+        }
+
+        public BitmapData LockImage()
+        {
+            Rectangle bounds = new Rectangle(Point.Empty, workingBitmap.Size);
+
+            width = (int)(bounds.Width * 4 + 3) & ~3;
+
+
+            bitmapData = workingBitmap.LockBits(bounds, ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            pBase = (Byte*)bitmapData.Scan0.ToPointer();
+            return bitmapData;
+        }
+
+        private uint* pixelData = null;
+
+        public uint GetPixel(int x, int y)
+        {
+            pixelData = (uint*)(pBase + y * width + x * 4);
+            return *pixelData;
+        }
+
+        public uint GetNextPixel()
+        {
+            return *++pixelData;
+        }
+
+        public void GetPixelArray(int x, int y, uint[] Values, int offset, int count)
+        {
+            pixelData = (uint*)(pBase + y * width + x * 4);
+            while (count-- > 0)
+            {
+                Values[offset++] = *pixelData++;
+            }
+        }
+
+        public void SetPixel(int x, int y, uint color)
+        {
+            pixelData = (uint*)(pBase + y * width + x * 4);
+            *pixelData = color;
+        }
+
+        public void SetNextPixel(uint color)
+        {
+            *++pixelData = color;
+        }
+
+        public void UnlockImage()
+        {
+            workingBitmap.UnlockBits(bitmapData);
+            bitmapData = null;
+            pBase = null;
+        }
+    }
+
+    public class XorShift
+    {
+        private ulong x;
+
+        public XorShift(ulong seed)
+        {
+            x = seed;
+        }
+
+        public ulong next()
+        {
+            x ^= x >> 12; // a
+            x ^= x << 25; // b
+            x ^= x >> 27; // c
+            return x * 7456821369425831467L;
+        }
+    }
+}
